292e0c3edfc399d7df3fc3aa4c4241a3d19169ad
llien30 <mana.smiles@gmail.com>
2020-05-22 12:00:11 +0900

    add -R filr

---
+++ Week1/homework1-R.py
@@ -0,0 +1,86 @@
+import sys
+from typing import List, Tuple
+
+
+def make_lower_sorted_word(word: str) -> str:
+    lower_word = word.lower()
+    sorted_word = "".join(sorted(list(lower_word)))
+    return sorted_word
+
+
+def make_sorted_dict(dictionary: List[str]) -> List[Tuple[str, str]]:
+    new_dictionary = []
+
+    # それぞれの単語について文字をソートして新しい辞書に追加
+    for word in dictionary:
+        sorted_word = make_lower_sorted_word(word)
+        new_dictionary.append((sorted_word, word))
+
+    new_dictionary.sort(key=lambda x: x[0])
+
+    return new_dictionary
+
+
+def bisect(right: int, left: int, word: str, dictionary: List[Tuple[str, str]]) -> str:
+    middle = (right + left) // 2
+    mid_word = dictionary[middle][0]
+
+    # 終了条件
+    if right - left <= 1:
+        if dictionary[middle][0] == word:
+            return dictionary[middle][1]
+        else:
+            return "Anagram does not exist in this dictionary"
+
+    # 答えがmiddleより左側にある場合、rightをmiddleに変更
+    elif word < mid_word:
+        right = middle
+        return bisect(right, left, word, dictionary)
+
+    # 答えがmiddleより右側にある場合、leftをmiddleに変更
+    else:
+        left = middle
+        return bisect(right, left, word, dictionary)
+
+
+def search_anagram(word: str, dictionary: List[Tuple[str, str]]) -> str:
+    first_word = dictionary[0][0]
+    last_word = dictionary[-1][0]
+
+    n_words = len(dictionary)
+    sorted_word = make_lower_sorted_word(word)
+
+    # bisectはリストの最初と最後を探すのがうまくいかないので別に確認しておく
+    if sorted_word == first_word:
+        anagram = dictionary[0][1]
+        return anagram
+    elif sorted_word == last_word:
+        anagram = dictionary[-1][1]
+        return anagram
+
+    # それ以外の場合は辞書を二分探索
+    anagram = bisect(n_words - 1, 0, sorted_word, dictionary)
+    return anagram
+
+
+def main() -> None:
+    sys.setrecursionlimit(20000000)
+
+    # 辞書の読み込み
+    with open("dictionary.txt") as f:
+        dictionary = [s.strip() for s in f.readlines()]
+
+    sorted_dictionary = make_sorted_dict(dictionary)
+
+    print("Ready to search Anagram")
+    print('Enter "q" to stop this program')
+    while True:
+        in_word = input()
+        if in_word == "q":
+            return
+        anagram = search_anagram(in_word, sorted_dictionary)
+        print(anagram)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Week1/homework2-R.py
@@ -0,0 +1,109 @@
+import argparse
+
+from collections import defaultdict
+from typing import DefaultDict, List, Tuple
+
+
+def get_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dictionary", type=str, default="dictionary.txt", help="dictionary file name"
+    )
+    return parser.parse_args()
+
+
+def make_letter2point() -> DefaultDict[str, int]:
+    letter2point = defaultdict(lambda: 1)
+    for letter in ["c", "f", "h", "l", "m", "p", "v", "w", "y"]:
+        letter2point[letter] += 1
+    for letter in ["j", "k", "q", "x", "z"]:
+        letter2point[letter] += 2
+    return letter2point
+
+
+def make_sorted_count_dict(
+    dictionary: List[str], letter2point: DefaultDict[str, int]
+) -> List[Tuple[DefaultDict[str, int], int, str]]:
+    # dog -> Tuple[DefaultDict{'d':1, 'g':1, 'o':1}, point ,dog]
+
+    count_dictionary = []
+
+    # それぞれの単語について文字数カウント,ポイント計算をして新しい辞書に追加
+    for word in dictionary:
+
+        lower_word = word.lower()
+        point = 0
+
+        # 単語内の文字数をカウント
+        word_dict = defaultdict(int)
+        for letter in lower_word:
+            word_dict[letter] += 1
+            point += letter2point[letter]
+
+        # その単語にqがある時はuの個数を1減らす
+        word_dict["u"] -= word_dict["q"]
+        point -= letter2point["u"] * word_dict["q"]
+
+        count_dictionary.append((word_dict, point, word))
+
+    # ポイントが高い順に辞書をソート
+    count_dictionary.sort(key=lambda x: x[1], reverse=True)
+
+    return count_dictionary
+
+
+def search_best_anagram(
+    input_word: str,
+    dictionary: List[Tuple[DefaultDict[str, int], str]],
+    letter2point: DefaultDict[str, int],
+) -> str:
+
+    lower_input_word = list(input_word.lower())
+
+    # 単語内の文字数をカウント
+    input_word_dict = defaultdict(int)
+    for letter in lower_input_word:
+        input_word_dict[letter] += 1
+
+    # その単語にqがある時はuの個数を1減らす
+    input_word_dict["u"] -= input_word_dict["q"]
+
+    # 辞書内の全単語探索、一番最初に見つかったアナグラムを返す
+    anagram = "Anagram does not exist in this dictionary"
+    for word_dict, point, word in dictionary:
+        letters = word_dict.keys()
+        exists = True
+        for letter in letters:
+            if input_word_dict[letter] < word_dict[letter]:
+                exists = False
+                break
+
+        if exists:
+            anagram = word
+            return anagram
+
+
+def main() -> None:
+    args = get_arguments()
+
+    # 辞書の読み込み
+    with open(args.dictionary) as f:
+        dictionary = [s.strip() for s in f.readlines()]
+
+    letter2point = make_letter2point()
+
+    count_dict = make_sorted_count_dict(dictionary, letter2point)
+
+    print("Ready to search Anagram")
+    print('Enter "q" if you want to stop program')
+
+    while True:
+        in_word = input().lower()
+        if in_word == "q":
+            return
+        anagram = search_best_anagram(in_word, count_dict, letter2point)
+        print(anagram)
+
+
+if __name__ == "__main__":
+    main()
