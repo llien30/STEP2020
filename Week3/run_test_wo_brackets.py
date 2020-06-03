09175b1ab8c55e1fc49fd971c029ba9dd1d94d29
llien30 <mana.smiles@gmail.com>
2020-06-03 23:46:42 +0900

    add test

---
+++ Week3/run_test_wo_brackets.py
@@ -0,0 +1,33 @@
+from op_parts_wo_brackets import tokenize, evaluate
+from calc_test import test
+
+
+def runTest():
+    print("==== Test started! ====")
+    test("1")
+    test("1+2")
+    test("1.0+2.1-3")
+    test("2*3")
+    test("2/3")
+    test("1+2*3")
+    test("1+2/3")
+    test("2*3/5")
+    test("1+2*3-2/3")
+    test("1.2+2.1*2.5/5.6-3.0")
+    print("==== Test finished! ====\n")
+
+
+def main():
+    runTest()
+    while True:
+        print("> ", end="")
+        line = input()
+        tokens = tokenize(line)
+        answer = evaluate(tokens)
+        print("answer = %f\n" % answer)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
